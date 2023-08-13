d9da60cd7c4c6d834a17fb97402937f69ef3c9ae
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2023-08-13 17:23:24 +0530

    add something

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String args[]) {
         System.out.println("hello world");
+        System.out.println("hi");
+        System.out.println("hello");
+
     }
 }
